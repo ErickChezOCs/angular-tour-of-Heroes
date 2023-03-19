6fb1141e31346f81c25bf229ed9e0e632ebf088f
ErickChezOCs <nounga72@yahoo.fr>
2023-03-20 00:45:39 +0100

    added heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id:12,name:'Cécile Fatiman'},
+    {id:13,name:'Manbo Marinette'},
+    {id:14,name:'Jean François Biassou'},
+    {id:15,name:'Toussaint Louverture'},
+    {id:16,name:'Henry Christophe'},
+    {id:17,name:'Jeannot Bullet'},
+    {id:18,name:'Franswa Makandal'},
+    {id:19,name:'Jean Jacques Dessalines'},
+    {id:20,name:'Capois La Mort'},
+]

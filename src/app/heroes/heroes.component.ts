7e5fcf6dda1315dd9318e2da762fbd118748b450
ErickChezOCs <nounga72@yahoo.fr>
2023-03-22 16:55:00 +0100

    before we add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 //import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,19 +12,30 @@ import { HeroService } from '../hero.service';
 })
 //, private messageService:MessageService
  
-export class HeroesComponent {
-heroes : Hero[];
-constructor(private heroService: HeroService) {}
+export class HeroesComponent implements OnInit{
+
+selectedHero?: Hero;
+
+heroes : Hero[] = [];
+
+constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+ngOnInit(): void{
+  this.getHeroes();
+}
+
+onSelect(hero:Hero): void {
+  this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+}
+
+
 getHeroes(): void {
    this.heroService.getHeroes()
    .subscribe(heroes => this.heroes = heroes);
 }
 
-ngOnInit(): void{
-  this.getHeroes();
-}
-selectedHero?:Hero;
-onSelect(hero:Hero): void {
-  this.selectedHero = hero;
-}
+
+ 
+
 }
